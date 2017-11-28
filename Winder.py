5819eea95efb3b5013c044c9bdc640283ebf256c
chas <yz80nomo@gmail.com>
2017-11-28 02:46:54 +0000

    fixing issues found during testing

--- Winder.py
+++ Winder.py
@@ -16,49 +16,49 @@ class Winder(object):
         self.rpm = rpm
         self.turn_type = turn_type
         self.rot_count = 0
-        pause_interval(self):
+        self.pause_interval()
 
     def turn_off_motors(self):
-        motor_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-        motor_hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-        motor_hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-        motor_hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+        self.motor_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        self.motor_hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        self.motor_hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        self.motor_hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
     def create_motor_hat(self):
         self.motor_hat = Adafruit_MotorHAT()
 
     def create_stepper(self):
-        self.my_stepper = stepper_motor.getStepper(self.steps_per_rev, self.motor_port)
-        self.my_stepper.setSpeed(self.rot_rate)
+        self.my_stepper = self.motor_hat.getStepper(self.steps_per_rev, self.motor_port)
+        self.my_stepper.setSpeed(self.rpm)
 
     def pause_interval(self):
-        self.pause = (60 * 60 * 24) / (rotations_per_day / rots_between_pause)
+        self.pause = (60 * 60 * 24) / (self.turns_per_day / self.turns_between_pause)
 
     def rotate_watch(self):
-        create_motor_hat(self)
-        create_stepper(self)
+        self.create_motor_hat()
+        self.create_stepper()
         i = 0
-        while(i < turns_between_pause):
-            if (rotation_type == "CW"):
-                stepper.step(steps_per_rev, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
-            elif (rotation_type == "CCW"):
-                stepper.step(steps_per_rev, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
-            elif (rotation_type == "ALT" and global_count % 2 == 0):
-                stepper.step(steps_per_rev, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
-            elif (rotation_type == "ALT" and global_count % 2 == 1):
-                stepper.step(steps_per_rev, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        while(i < self.turns_between_pause):
+            if (self.turn_type == "CW"):
+                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
+            elif (self.turn_type == "CCW"):
+                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+            elif (self.turn_type == "ALT" and self.rot_count % 2 == 0):
+                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
+            elif (self.turn_type == "ALT" and self.rot_count % 2 == 1):
+                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
             else:
                 print("Bad config.")
-            rot_count = rot_count + 1
+            self.rot_count = self.rot_count + 1
             i = i + 1
-        turn_off_motors(self)
+        self.turn_off_motors()
 
     # Needed so that motor hat can be killed outside of class.
-    def get_motor_hat():
-        return motor_hat
+    def get_motor_hat(self):
+        return self.motor_hat
 
-    def get_turn_count():
-        return rot_count
+    def get_turn_count(self):
+        return self.rot_count
 
-    def get_pause_interval():
-        return pause
+    def get_pause_interval(self):
+        return self.pause
--- watchwinder.py
+++ watchwinder.py
@@ -4,7 +4,7 @@
 
 import time
 import atexit
-import Winder
+from Winder import Winder
 
 rots_per_day = 750
 rot_rate = 30
@@ -13,4 +13,5 @@ rot_type = "ALT"
 winder = Winder(rots_per_day, rot_rate, rot_type)
 while (winder.get_turn_count() < rots_per_day):
     winder.rotate_watch()
+    print(winder.get_pause_interval())
     time.sleep(winder.get_pause_interval())
