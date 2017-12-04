8766f3b3ce7104c194e980a1e61d9c9d442e0ed5
Chas Berndt <yz80nomo@gmail.com>
2017-12-03 21:05:36 -0500

    Adding print statement to get turn count.

--- watchwinder.py
+++ watchwinder.py
@@ -14,4 +14,5 @@ winder = Winder(rots_per_day, rot_rate, rot_type)
 while (winder.get_turn_count() < rots_per_day):
     winder.rotate_watch()
     print(winder.get_pause_interval())
+    print(winder.get_turn_count())
     time.sleep(winder.get_pause_interval())
