e56d8a4152e12a0e8bdbc49c0daf3283cd575b89
Chas Berndt <yz80nomo@gmail.com>
2017-12-03 20:37:02 -0500

    Adding over turn configuration to class. This will cause the watch to turn more than a full rotation per turn to ensure a positive turn.

--- Winder.py
+++ Winder.py
@@ -7,9 +7,10 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_StepperMotor
 class Winder(object):
 
     # Constants for this stepper motor. These might need to be moved later.
-    motor_port = 1          # Port that stepper motor is connected to.
-    steps_per_rev = 200     # Number of steps/revolution for stepper motor.
-    turns_between_pause = 5  # Number of turns before pause and/or reverse direction.
+    motor_port = 1              # Port that stepper motor is connected to.
+    steps_per_rev = 200         # Number of steps/revolution for stepper motor.
+    over_turn = 0.25            # Amount of overturn per revolution.
+    turns_between_pause = 5     # Number of turns before pause and/or reverse direction.
 
     def __init__(self, turns_per_day, rpm, turn_type):
         self.turns_per_day = turns_per_day
@@ -17,6 +18,7 @@ class Winder(object):
         self.turn_type = turn_type
         self.rot_count = 0
         self.pause_interval()
+        self.steps_per_turn()
 
     def turn_off_motors(self):
         self.motor_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
@@ -34,19 +36,22 @@ class Winder(object):
     def pause_interval(self):
         self.pause = (60 * 60 * 24) / (self.turns_per_day / self.turns_between_pause)
 
+    def steps_per_turn(self):
+        self.total_steps = int(steps_per_rev * (1 + self.over_turn))
+
     def rotate_watch(self):
         self.create_motor_hat()
         self.create_stepper()
         i = 0
         while(i < self.turns_between_pause):
             if (self.turn_type == "CW"):
-                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
+                self.my_stepper.step(self.total_steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
             elif (self.turn_type == "CCW"):
-                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+                self.my_stepper.step(self.total_steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
             elif (self.turn_type == "ALT" and self.rot_count % 2 == 0):
-                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
+                self.my_stepper.step(self.total_steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
             elif (self.turn_type == "ALT" and self.rot_count % 2 == 1):
-                self.my_stepper.step(self.steps_per_rev, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+                self.my_stepper.step(self.total_steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
             else:
                 print("Bad config.")
             self.rot_count = self.rot_count + 1
